e345efea753fcf7f1216d658d131499f915182a4
Pranav Balgi <14875026+pgbalgi@users.noreply.github.com>
2023-06-11 13:28:40 -0700

    Optimized prediction

--- flyingsquid/label_model.py
+++ flyingsquid/label_model.py
@@ -67,10 +67,7 @@ class LabelModel(_triplets.Mixin, _graphs.Mixin, _observables.Mixin,
 
 
     def _compute_moment(self, lambda_i, lambda_j):
-        non_abstain_i = np.unique(np.where(lambda_i[:,] != 0)[0])
-        non_abstain_j = np.unique(np.where(lambda_j[:,] != 0)[0])
-        non_abstain = np.intersect1d(non_abstain_i, non_abstain_j)
-
+        non_abstain = np.nonzero(np.prod(lambda_i * lambda_j, axis=-1))
         lambda_i = lambda_i[non_abstain]
         lambda_j = lambda_j[non_abstain]
 
@@ -80,27 +77,32 @@ class LabelModel(_triplets.Mixin, _graphs.Mixin, _observables.Mixin,
     def predict_proba(self, L_matrix):
         '''Predict the probabilities of the Y's given the outputs of the LF's.
         
-        L_matrix: a m x d matrix of of LF outputs. L_matrix[k][i] is the value of \lambda_i on item k.
+        L_matrix: a n x m x d matrix of of LF outputs. L_matrix[k][i] is the value of \lambda_i on item k.
             1 means positive, -1 means negative, 0 means abstain.
                 
         Outputs: a d vector of probabilities
         '''
-        m, d = L_matrix.shape
-        proba = np.zeros(d)
+        n, m, d = L_matrix.shape
+        proba = np.zeros((n,d))
 
-        abstain = np.unique(np.where(L_matrix[:,] == 0)[0])
+        abstain = np.nonzero(np.prod(L_matrix, axis=-1) == 0)
 
         for c in range(d):
             y = np.zeros(d)
             y[c] = 1
 
-            prob_c = (1 + (L_matrix @ y) * self.acc) / 2
+            match = L_matrix @ y
+            # mismatches = np.nonzero(match == -1)
+
+            prob_c = (1 + match * self.acc) / 2
+            # prob_c[mismatches] *= self.class_balance / (1 - self.class_balance[c])
             prob_c *= 1 - self.abstain_rate
-            prob_c[abstain] = self.abstain_rate[abstain]
+            prob_c[abstain] = self.abstain_rate[abstain[-1]]
 
-            proba[c] = np.prod(prob_c) * self.class_balance[c]
+            proba[:,c] = np.prod(prob_c, axis=-1) * self.class_balance[c]
 
-        proba /= np.sum(proba)
+
+        proba /= np.sum(proba, axis=-1, keepdims=True)
 
         return proba
     
@@ -116,9 +118,8 @@ class LabelModel(_triplets.Mixin, _graphs.Mixin, _observables.Mixin,
         n, m, d = L_matrix.shape
         one_hot_pred = np.zeros((n,d))
 
-        for i in range(n):
-            pred = np.argmax(self.predict_proba(L_matrix[i]), axis=-1)
-            one_hot_pred[i,pred] = 1
+        pred = np.argmax(self.predict_proba(L_matrix), axis=-1)
+        one_hot_pred[range(n), pred] = 1
 
         return one_hot_pred
     
